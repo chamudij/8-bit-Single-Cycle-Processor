1c4e0d9e008ca6053798fa893ba40959347af05f
chamudij <chamudij99@gmail.com>
2023-06-08 08:05:46 +0530

    Part 5 added

---
+++ Part 5/alu.v
@@ -0,0 +1,97 @@
+//ALU
+module alu(DATA1,DATA2,ZERO,RESULT,SELECT,SHIFT);
+	//Port declaration
+	input [7:0] DATA1;			        //8 bit unsigned input 
+	input signed [7:0] DATA2;			//8 bit signed input
+	input [2:0] SELECT;             	//3 bit unsigned input
+	input [1:0] SHIFT;                  //2 bit unsigned input
+	output reg signed[7:0] RESULT;		//8 bit signed input register
+	output reg[0:0] ZERO;
+	
+	wire [7:0] FORWARD_RESULT,AND_RESULT,OR_RESULT,ADD_RESULT,ROR_RESULT,SLL_RESULT,SRL_RESULT,SRA_RESULT; //8 bit wires to collect design outputs
+
+	//instantiate FORWARD module
+	FORWARD myforward(DATA2,FORWARD_RESULT);
+	//instantiate AND module	
+	AND myand(DATA1,DATA2,AND_RESULT);
+	//instantiate OR module
+	OR myor(DATA1,DATA2,OR_RESULT);
+	//instantiate ADD module
+	ADD myadd(DATA1,DATA2,ADD_RESULT);
+
+	/*******/
+	//instatiate ROR,SLL,SRL,SRA modules
+	ROR myror(ROR_RESULT,DATA1,DATA2);
+	SLL mysll(SLL_RESULT,DATA1,DATA2);
+	SRL mysrl(SRL_RESULT,DATA1,DATA2);
+	SRA mysra(SRA_RESULT,DATA1,DATA2);
+	/******/
+	
+	//Get executed whenever the values SELECT,FORWARD_RESULT,AND_RESULT,ADD_RESULT and OR_RESULT changes
+	always @(*)
+	//Select the result based on the value of the selecct signal
+	begin
+		case(SELECT)
+			3'b000 : RESULT = FORWARD_RESULT; 
+			3'b001 : RESULT = ADD_RESULT;
+			3'b010 : RESULT = AND_RESULT;
+			3'b011 : RESULT = OR_RESULT;
+			3'b100 : begin
+						if(ADD_RESULT == 0) begin
+							ZERO = 1'b1;
+						end else begin
+							ZERO = 1'b0;
+						end
+					end
+					
+			/********/
+			3'b101 : RESULT = ROR_RESULT;
+			3'b110 : begin
+						if(SHIFT == 2'b01) begin
+							RESULT = SLL_RESULT;
+						end else if(SHIFT == 2'b10) begin
+							RESULT = SRL_RESULT;
+						end else if(SHIFT == 2'b11) begin
+							RESULT = SRA_RESULT;
+						end
+					end
+
+			default : RESULT = 8'b0;
+			/********/
+			
+		endcase
+	end	
+endmodule	
+
+//module for FORWARD function
+module FORWARD(IN,OUT);
+	input [7:0] IN;		//8 bit input
+	output [7:0] OUT;	//8 bit output
+	
+	assign #1 OUT = IN; //assign input to output with 1 unit delay
+endmodule	//FORWARD
+
+//module for AND function
+module AND(IN1,IN2,OUT);
+	input [7:0] IN1,IN2; //8 bit inputs
+	output [7:0] OUT;	 //8 bit inputs
+	
+	assign #1 OUT = IN1 & IN2; //bitwise and on inputs with 1 unit delay
+endmodule	//AND
+
+//module for OR function
+module OR(IN1,IN2,OUT);
+	input [7:0] IN1,IN2; //8 bit inputs
+	output [7:0] OUT;    //8 bit output
+	
+	assign #1 OUT = IN1 | IN2;	//bitwise or on inputs with 1 unit delay
+endmodule	//OR
+
+//module for ADD function
+module ADD(IN1,IN2,OUT);
+	input [7:0] IN1;			//8 bit unsigned input 
+	input signed [7:0] IN2;		//8 bit signed input 
+	output signed [7:0] OUT;	//8 bit signed output 
+	
+	assign #2 OUT=IN1+IN2;	//addition of inputs with 2 unit delay
+endmodule	//ADD
---
+++ Part 5/sll.v
@@ -0,0 +1,44 @@
+
+module SLL(OUTPUT,IN,SHIFT); //module to logical shift left by  given SHIFT value
+	input[7:0] IN,SHIFT;	//shifting value will be maximum 7 for 8-bit value
+	output[7:0] OUTPUT;		
+	reg [2:0]SEL;
+	wire [7:0] OUT;
+	
+	always@(SHIFT) begin
+	case(SHIFT)
+	//here depending on the shifting value it assigns values to select signals
+		8'b000: 
+			SEL = 3'b000;
+		8'b001 : 
+			SEL = 3'b001;
+		8'b010 : 
+			SEL = 3'b010;
+		8'b011 : 
+			SEL = 3'b011;	
+		8'b100 : 
+			SEL = 3'b100;
+		8'b101 : 
+			SEL = 3'b101;
+		8'd110 : 
+			SEL = 3'b110;
+		8'd111 : 
+			SEL = 3'b111;
+	endcase
+	end
+	//here mux 8 represents least significant bit of the value after left shifted and
+	//mux 1 represents most significant bit of the value after left shifted 
+	//after shifting to the left it's padding out with 0's(least significant bits)
+
+
+	MUX8x1 mux1(OUT[7],IN[7],IN[6],IN[5],IN[4],IN[3],IN[2],IN[1],IN[0],SEL);
+	MUX8x1 mux2(OUT[6],IN[6],IN[5],IN[4],IN[3],IN[2],IN[1],IN[0],1'b0,SEL);
+	MUX8x1 mux3(OUT[5],IN[5],IN[4],IN[3],IN[2],IN[1],IN[0],1'b0,1'b0,SEL);
+	MUX8x1 mux4(OUT[4],IN[4],IN[3],IN[2],IN[1],IN[0],1'b0,1'b0,1'b0,SEL);
+	MUX8x1 mux5(OUT[3],IN[3],IN[2],IN[1],IN[0],1'b0,1'b0,1'b0,1'b0,SEL);
+	MUX8x1 mux6(OUT[2],IN[2],IN[1],IN[0],1'b0,1'b0,1'b0,1'b0,1'b0,SEL);
+	MUX8x1 mux7(OUT[1],IN[1],IN[0],1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,SEL);
+	MUX8x1 mux8(OUT[0],IN[0],1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,SEL);
+	
+	assign #2 OUTPUT = OUT;
+endmodule
